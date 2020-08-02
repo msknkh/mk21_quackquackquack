c4f576efd8831a8ad48d637d63bd1acd72147c64
Shrey Jain <shrey.jain711@gmail.com>
2020-08-02 09:50:13 +0530

    setup screens for all pages in app

---
+++ frontend/components/Tutorial.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container, Text} from 'native-base';
+
+class Tutorial extends React.Component{
+    render()
+    {
+        return(
+            <Container>
+                <Text>
+                    Tutorial Videos!
+                </Text>
+            </Container>
+        );
+    }
+}
+
+export default Tutorial;
